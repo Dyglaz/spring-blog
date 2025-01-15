08006cc6b7e9b005684fc201011054248e1ee212
Dyglaz <kallkae@mail.ru>
2025-01-15 15:29:20 +0300

    initial commit

---
+++ src/test/java/com/kiselev/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiselev.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
